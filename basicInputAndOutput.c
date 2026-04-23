b4139cfdac98fef3f8467dc47c920e8d9fe1abe7
md-suhag <abdussalamsuhag104@gmail.com>
2026-04-23 16:30:49 +0600

    add: basic i/o code

---
+++ basicInputAndOutput.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int age;
+    printf("Enter your age:");
+    
+    scanf("%d", &age);
+    
+    printf("Age is %d", age);
+
+return 0;
+}
