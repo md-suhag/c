1cee1eaf2892fcd21bce05d004220d516698bba5
md-suhag <abdussalamsuhag104@gmail.com>
2026-04-23 16:52:27 +0600

    add: fgets()

--- basicInputAndOutput.c
+++ basicInputAndOutput.c
@@ -25,12 +25,42 @@
 /*
 Reading a Character
 */
-int main() {
-    char ch;
-    printf("Enter your character: \n");
+// int main() {
+//     char ch;
+//     printf("Enter your character: \n");
 
-    scanf("%c", &ch);
-    printf("you entered %c", ch);
+//     scanf("%c", &ch);
+//     printf("you entered %c", ch);
+
+// return 0;
+// }
+
+
+/*
+Reading a string
+*/
+// int main() {
+//     char str[100];
+
+//     printf("Enter your string:");
+
+//     scanf("%s", str);
+//     printf("you entered %s", str);
+
+// return 0;
+// }
+
+/*
+fgets()
+*/
+int main() {
+    char name[20];
+
+    printf("Enter your name:");
+
+    fgets(name, sizeof(name), stdin);
+
+    printf("Hello,  %s", name);
 
 return 0;
 }
