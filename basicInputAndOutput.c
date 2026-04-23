787d6f54bd9a8806de0ea59a780928a10959ce91
md-suhag <abdussalamsuhag104@gmail.com>
2026-04-23 16:41:42 +0600

    add: reading a character

--- basicInputAndOutput.c
+++ basicInputAndOutput.c
@@ -1,13 +1,36 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 
+// int main() {
+//     int age;
+//     printf("Enter your age:");
+    
+//     scanf("%d", &age);
+    
+//     printf("Age is %d", age);
+
+// return 0;
+// }
+
+/*
+  fputs()
+*/
+// int main() {
+//     fputs("This is my string", stdout);
+
+// return 0;
+// }
+
+
+/*
+Reading a Character
+*/
 int main() {
-    int age;
-    printf("Enter your age:");
-    
-    scanf("%d", &age);
-    
-    printf("Age is %d", age);
+    char ch;
+    printf("Enter your character: \n");
+
+    scanf("%c", &ch);
+    printf("you entered %c", ch);
 
 return 0;
 }
